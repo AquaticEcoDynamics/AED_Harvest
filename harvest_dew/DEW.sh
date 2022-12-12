9e1752127f35dbf0be3dcd8a2ca5cacb571a69ae
Casper Boon <casper.boon@uwa.edu.au>
2022-12-12 13:31:36 +0800

    update harvest_dew

--- harvest_dew/DEW.sh
+++ harvest_dew/DEW.sh
@@ -1,43 +1,17 @@
-#!/bin/bash
+#!/bin/sh
 #
 # harvest_dew/DEW.sh
 #
 . ./common/start.sh
 
-FILE=/tmp/tmpx$$
+export MYLOGFILE="${LOGDIR}/`date +'%Y%m%d%H%M'`.log"
 
-SITECODE=A4261002
-UNIT_ID=241
+export FILE=/tmp/tmpx$$
 
+export SRCHDATE="${YEAR}-${MONTH}-${DAY}"
 
-DATADIR="data/${YEAR}/harvest_dew/${SITECODE}"
-
-BASESITE="https://water.data.sa.gov.au/Export/BulkExport?"
-
-# Curiously, asking for 100days of data returns way more - all the way back to 2011
-#QUERY="DateRange=Days7"
-#QUERY="DateRange=Days100"
-QUERY="DateRange=Custom"
-QUERY=${QUERY}"&StartTime=${YYEAR}-${YMONTH}-${YDAY}%2000%3A00"
-QUERY=${QUERY}"&EndTime=${TYEAR}-${TMONTH}-${TDAY}%2023%3A59"
-QUERY=${QUERY}"&TimeZone=9.5"
-QUERY=${QUERY}"&Calendar=CALENDARYEAR"
-#QUERY=${QUERY}"&Interval=Daily"
-QUERY=${QUERY}"&Interval=Hourly"
-QUERY=${QUERY}"&Step=1&ExportFormat=csv"
-QUERY=${QUERY}"&TimeAligned=True"
-QUERY=${QUERY}"&RoundData=False"
-QUERY=${QUERY}"&IncludeGradeCodes=False"
-QUERY=${QUERY}"&IncludeApprovalLevels=False"
-QUERY=${QUERY}"&IncludeInterpolationTypes=False"
-QUERY=${QUERY}"&Datasets[0].DatasetName=Discharge.Total%20barrage%20flow%40${SITECODE}"
-QUERY=${QUERY}"&Datasets[0].Calculation=Aggregate"
-QUERY=${QUERY}"&Datasets[0].UnitId=${UNIT_ID}"
-QUERY=${QUERY}"&_=1636421128724"
-URL="${BASESITE}${QUERY}"
-
-#echo ${QUERY}
-#echo $URL
+#export BASESITE="https://water.data.sa.gov.au/Export/BulkExport?"
+export BASESITE="https://water.data.sa.gov.au/Export/DataSet?"
 
 # a small subroutine to convert date data from "YYYY-mm-dd HH:MM" to
 # isodate format (YYYYMMDDHHmm) for easy comparison
@@ -58,45 +32,130 @@ makeiso () {
 }
 
 
-SRCHDATE="${YEAR}-${MONTH}-${DAY}"
-ARCHIVEF="${DATADIR}/${TODAY}.csv"
+process_site ()
+{
+  SITECODE="$1"
+  UNIT_ID="$2"
+  DATASET="$3"
+  URL="$4"
+  URL="${URL}&_=1636421128724"
 
+# echo SITE "$SITECODE"  DATASET "$DATASET" UNIT_ID "$UNIT_ID"
 
-# Fetch the data file
-wget -q $URL --user-agent="" -O $FILE
-#cat $FILE
-#/bin/rm $FILE
-#exit
+  DATADIR="data/${YEAR}/harvest_dew/${SITECODE}-`echo $DATASET | sed -e 's/%20/_/g'`"
 
-if [ $? -eq 0 ] ; then
-  if [ -f $FILE ] ; then
-    grep "^${SRCHDATE}" $FILE | while read LINE ; do
-#     echo \"${LINE}\"
-      if [ "$LINE" != "" ] ; then
-        DATE=`echo $LINE | cut -f1 -d,`
-        FLOW=`echo $LINE | cut -f3 -d,` 
-        if [ $FLOW != "NaN" ] ; then
-          if [ ! -f ${ARCHIVEF} ] ; then
-            if [ ! -d ${DATADIR} ] ; then
-              mkdir -p ${DATADIR}
-            fi
-            echo "Date,Flow" > ${ARCHIVEF}
-          fi
-          echo "${DATE}, $FLOW" >> ${ARCHIVEF}
-          log_last_update
-        fi
-#     else
-#       echo no line
-      fi
-    done
+  ARCHIVEF="${DATADIR}/${TODAY}.csv"
 
-    # if we got it off the net we can remove it
-    /bin/rm $FILE
-# else
-#   echo no file
+  RIGHTNOW=`date "+%Y-%m-%d %H:%M:%S"`
+  DAYNOW=`date "+%Y%m%d"`
+  URLVERS=1
+
+  # Fetch the data file
+  curl -X GET "${URL}" -s -o $FILE
+  if [ $? -ne 0 ] ; then
+    URLVERS=2
+
+    QUERY="DataSet=${DATASET}%40${SITECODE}"
+    # If now start/end time specified it should return full data set
+#   if [ "$TODAY" = "$DAYNOW" ] ; then
+#     QUERY=${QUERY}"&StartTime=${YYEAR}-${YMONTH}-${YDAY}%2000%3A00"
+#     QUERY=${QUERY}"&EndTime=${TYEAR}-${TMONTH}-${TDAY}%2023%3A59"
+#   fi
+    # This is much quicker than "The whole set" but will fail on backfill before last week
+    QUERY=${QUERY}"&DateRange=Days7"
+    QUERY=${QUERY}"&ExportFormat=csv"
+    QUERY="${QUERY}&Compressed=false"
+    QUERY=${QUERY}"&RoundData=False"
+    QUERY=${QUERY}"&TimeZone=9.5"
+    URL="${BASESITE}${QUERY}"
+
+    curl -X GET "${URL}" -s -o $FILE
   fi
-#else
-# echo fetch failed
-fi
+
+  if [ $? -eq 0 ] ; then
+
+    echo $RIGHTNOW fetch OK with form $URLVERS url for SITE "$SITECODE" DATASET "$DATASET" UNIT_ID "$UNIT_ID" >> ${MYLOGFILE}
+
+    if [ -f $FILE ] ; then
+      grep "^${SRCHDATE}" $FILE | while read LINE ; do
+        if [ "$LINE" != "" ] ; then
+          DATE=`echo $LINE | cut -f1 -d,`
+          VALU=`echo $LINE | cut -f3 -d,`
+          if [ $VALU != "NaN" ] ; then
+            if [ ! -f ${ARCHIVEF} ] ; then
+              if [ ! -d ${DATADIR} ] ; then
+                mkdir -p ${DATADIR}
+              fi
+              echo "Date,Value" > ${ARCHIVEF}
+            fi
+            echo "${DATE}, $VALU" >> ${ARCHIVEF}
+            log_last_update
+          fi
+        fi
+      done
+
+      # if we got it off the net we can remove it
+      /bin/rm $FILE
+      if [ -f ${ARCHIVEF} ] ; then
+        head -1 ${ARCHIVEF} > $FILE
+        tail -n+2 ${ARCHIVEF} | sort -u > $FILE
+        /bin/rm ${ARCHIVEF}
+        /bin/mv $FILE ${ARCHIVEF}
+      else
+        echo $RIGHTNOW no data for $TODAY '@' SITE "$SITECODE" DATASET "$DATASET" >> ${MYLOGFILE}
+      fi
+    fi
+  else
+    echo $RIGHTNOW fetch failed on both forms for SITE "$SITECODE"  DATASET "$DATASET" UNIT_ID "$UNIT_ID" >> ${MYLOGFILE}
+#   echo \"$4\" >> ${MYLOGFILE}
+#   echo \"$URL\" >> ${MYLOGFILE}
+  fi
+}
+
+
+#===============================================================================
+URL='https://water.data.sa.gov.au/Export/BulkExport?'
+URL="${URL}DateRange=Custom"
+URL="${URL}&StartTime=2022-12-10%2000%3A00"
+URL="${URL}&EndTime=2022-12-12%2023%3A59"
+URL="${URL}&TimeZone=9.5"
+URL="${URL}&Calendar=CALENDARYEAR"
+URL="${URL}&Interval=Hourly"
+URL="${URL}&Step=1"
+URL="${URL}&ExportFormat=csv"
+URL="${URL}&TimeAligned=True"
+URL="${URL}&RoundData=False"
+URL="${URL}&IncludeGradeCodes=False"
+URL="${URL}&IncludeApprovalLevels=False"
+URL="${URL}&IncludeInterpolationTypes=False"
+URL="${URL}&Datasets[0].DatasetName=Discharge.Total%20barrage%20flow%40A4261002"
+URL="${URL}&Datasets[0].Calculation=Aggregate"
+URL="${URL}&Datasets[0].UnitId=241"
+#URL="${URL}&_=1636421128724"
+
+SITECODE=A4261002
+UNIT_ID=241
+DATASET='Discharge.Total%20barrage%20flow'
+
+#NEXTONE=`date --date="-3mins" "+%Y%m%d%H%M%S"`
+
+NEXTONE=`date --date="+20secs" "+%Y%m%d%H%M%S"`
+process_site "$SITECODE" "$UNIT_ID" "$DATASET" "$URL"
+
+sort -u harvest_dew/theaddress.txt | while read LINE ; do
+   SITECODE=`echo $LINE | tr -d '\r' | cut -f1 -d,`
+   URL=`echo $LINE | tr -d '\r' | cut -f2- -d,`
+   DATASET=`echo $URL | tr '&' '\n' | grep DatasetName | cut -f2 -d= | sed -e 's/%40/\@/' | cut -f1 -d\@`
+   UNIT_ID=`echo $URL | tr '&' '\n' | grep UnitId | cut -f2 -d=`
+
+   THISONE=`date "+%Y%m%d%H%M%S"`
+   if [ $THISONE -lt $NEXTONE ] ; then
+     sleep 15
+   fi
+   NEXTONE=`date --date="+20secs" "+%Y%m%d%H%M%S"`
+   process_site "$SITECODE" "$UNIT_ID" "$DATASET" "$URL"
+done
 
 . ./common/finish.sh
+
+exit 0
